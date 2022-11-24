25d6045733df356ec80879eea5f3313ef0f4e340
porovoz <falkon1@list.ru>
2022-11-24 15:47:23 +0300

    Homework 13 is done! Please check.

--- src/homework12/Author.java
+++ src/homework12/Author.java
@@ -1,5 +1,7 @@
 package homework12;
 
+import java.util.Objects;
+
 public class Author {
     private final String firstName;
     private final String lastName;
@@ -15,5 +17,23 @@ public class Author {
 
     public String getLastName() {
         return lastName;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s", firstName, lastName);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return Objects.equals(firstName, author.firstName) && Objects.equals(lastName, author.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName);
     }
 }
--- src/homework12/Book.java
+++ src/homework12/Book.java
@@ -1,5 +1,7 @@
 package homework12;
 
+import java.util.Objects;
+
 public class Book {
     private final String name;
     private final Author authorName;
@@ -25,5 +27,24 @@ public class Book {
 
     public void setPublicationYear(int publicationYear) {
         this.publicationYear = publicationYear;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s, '%s', %d", authorName, name, publicationYear);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book3 = (Book) o;
+        return publicationYear == book3.publicationYear && Objects.equals(name, book3.name) && Objects.equals(authorName,
+                book3.authorName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, authorName, publicationYear);
     }
 }
